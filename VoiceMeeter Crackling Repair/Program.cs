59f0af376d072fd747b77ff72eb8bdbf72e206c5
TheRake66 <65202670+TheRake66@users.noreply.github.com>
2024-01-07 06:39:06 +0100

    first version

---
+++ VoiceMeeter Crackling Repair/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceMeeter_Crackling_Repair
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IntPtr mask = new IntPtr(1 << Environment.ProcessorCount - 1);
+                foreach (Process audio in Process.GetProcessesByName("audiodg"))
+                {
+                    audio.ProcessorAffinity = mask;
+                    audio.PriorityBoostEnabled = true;
+                    audio.PriorityClass = ProcessPriorityClass.High;
+                }
+                Console.WriteLine("Fixed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!" + Environment.NewLine + ex.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
